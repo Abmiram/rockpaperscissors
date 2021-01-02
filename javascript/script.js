cf1afc5b6532a973dab22ed5a723d6aca0a4288e
Ross Birdsall <ross.birdsall9@gmail.com>
2021-01-02 10:35:09 -0500

    simplified js

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,4 @@
 const choices = ["rock", "paper", "scissors"];
-const message = document.getElementById("gameResults");
-let userChoice = "";
 
 let randomChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -8,19 +6,16 @@ let randomChoice = () => {
 }
 
 const playGame = () => {
+    const message = document.getElementById("gameResults");
     let computerChoice = randomChoice();
-    console.log(`You chose ${userChoice} and the computer chose ${computerChoice}.`)
     if (userChoice === "rock" && computerChoice === "scissors"
     || userChoice === "paper" && computerChoice === "rock"
     || userChoice === "scissors" && computerChoice === "paper") {
         message.innerHTML = `You chose ${userChoice} and the computer chose ${computerChoice}.  You win!`
-        console.log("You win!");
     } else if (userChoice === computerChoice) {
         message.innerHTML = `You chose ${userChoice} and the computer chose ${computerChoice}.  It's a tie.`
-        console.log("It's a tie.");
     } else {
         message.innerHTML = `You chose ${userChoice} and the computer chose ${computerChoice}.  You lose :(`
-        console.log("You lose. :(");
     }
 }
 
