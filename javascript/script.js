26d99df93ad5a4504ff94ff3c94f26ea9d39d246
Ross Birdsall <ross.birdsall9@gmail.com>
2021-01-02 09:15:37 -0500

    attempted build npm packages

--- javascript/script.js
+++ javascript/script.js
@@ -9,22 +9,18 @@ let randomChoice = () => {
 
 const playGame = () => {
     let computerChoice = randomChoice();
-    console.log(`You chose ${userChoice} and the computer chose ${computerChoice}.`)
     if (userChoice === "rock" && computerChoice === "scissors"
     || userChoice === "paper" && computerChoice === "rock"
     || userChoice === "scissors" && computerChoice === "paper") {
         message.innerHTML = `You chose ${userChoice} and the computer chose ${computerChoice}.  You win!`
-        console.log("You win!");
     } else if (userChoice === computerChoice) {
         message.innerHTML = `You chose ${userChoice} and the computer chose ${computerChoice}.  It's a tie.`
-        console.log("It's a tie.");
     } else {
         message.innerHTML = `You chose ${userChoice} and the computer chose ${computerChoice}.  You lose :(`
-        console.log("You lose. :(");
     }
 }
 
-const getUserChoice = (selectedIcorn) => {
-    userChoice = `${selectedIcorn}`;
+const getUserChoice = (selectedIcon) => {
+    userChoice = `${selectedIcon}`;
     playGame();
 }
